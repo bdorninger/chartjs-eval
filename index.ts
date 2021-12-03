2fe1fd8c2a7fb180e676596d8cc9f085885dabf2
bdorninger <bernhard.dorninger@scch.at>
2021-12-03 14:43:27 +0100

    3 files modified

--- index.ts
+++ index.ts
@@ -1,6 +1,11 @@
 import './style.css';
 
-import { Chart, ChartOptions, ChartEvent, ChartTypeRegistry } from 'chart.js/auto';
+import {
+  Chart,
+  ChartOptions,
+  ChartEvent,
+  ChartTypeRegistry,
+} from 'chart.js/auto';
 // import { getRelativePosition } from 'chart.js/helpers';
 import * as dragData from 'chartjs-plugin-dragdata';
 
@@ -9,6 +14,9 @@ const DATA_COUNT = 7;
 const ctx = (
   document.getElementById('myChart') as HTMLCanvasElement
 ).getContext('2d');
+
+const tempRange = document.getElementById('tempRange');
+tempRange.addEventListener('change', onTempValueChange);
 
 const plugins = [dragData];
 Chart.register(plugins);
@@ -20,15 +28,28 @@ const config: ChartOptions = {
       {
         label: '  Temperature',
         data: [10, 13, 13, 15, 6, 9],
+        borderWidth: 2,
         borderColor: '#ff0016',
         backgroundColor: '#11aa88',
+      },
+      {
+        data: [11, 12.5, 12.8, 14, 4.4, 8.5],
+        borderWidth: 1,
+        borderColor: 'rgba(255,0,0,0.4)',
+        backgroundColor: 'rgba(255,0,0,0)',
+      },
+      {
+        data: [10.5, 12.3, 12.6, 13.8, 3, 7.9],
+        borderWidth: 1,
+        borderColor: 'rgba(255,0,0,0.1)',
+        backgroundColor: 'rgba(255,0,0,0)',
       },
       {
         label: '  Pressure',
         data: [12, 19, 3, 5, 2, 3],
         fill: false,
         tension: 0.2,
-        borderWidth: 3,
+        borderWidth: 4,
         borderColor: '#4dc9f6',
         backgroundColor: '#000000',
         pointHitRadius: 25,
@@ -39,7 +60,7 @@ const config: ChartOptions = {
         data: [7, 11, 5, 8, 3, 7],
         fill: false,
         tension: 0.4,
-        borderWidth: 3,
+        borderWidth: 6,
         borderColor: '#4dc900',
         backgroundColor: '#ffffff',
         pointHitRadius: 25,
@@ -53,9 +74,9 @@ const config: ChartOptions = {
         max: 20,
       },
     },
-    onHover: function (e : ChartEvent) {
-      const point = e.chart.getElementsAtEventForMode(
-        e,
+    onHover: function (e: ChartEvent) {
+      const point = chart.getElementsAtEventForMode(
+        e.native,
         'nearest',
         { intersect: true },
         false
@@ -63,7 +84,20 @@ const config: ChartOptions = {
       /*if (point.length) e.native.target.style.cursor = 'grab';
       else e.native.target.style.cursor = 'default';*/
     },
+    onClick: function (e: ChartEvent) {
+      console.log("Clicki")
+      // chart.getElementsAtEventForMode(e.native,'',)
+    },
     plugins: {
+      legend: {
+        labels: {
+          filter: function (item, chart) {
+            // Logic to remove a particular legend item goes here            
+            return item.text !== undefined;
+          },
+        },
+      },
+
       dragData: {
         round: 1,
         showTooltip: true,
@@ -84,3 +118,11 @@ const config: ChartOptions = {
 };
 
 const chart = new Chart(ctx, config);
+tempRange.setAttribute('value', String(chart.data.datasets[0].borderWidth));
+
+export function onTempValueChange(ev: any) {
+  ev.preventDefault();
+  console.log(`changed ${this}: ${ev.target.value}`);
+  chart.data.datasets[0].borderWidth = ev.target.value;
+  setTimeout(() => chart.update('none'), 100);
+}
